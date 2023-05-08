8386dfdaa12047a20a2295f48da4d85f95828a7f
Abhishek Kapoor <42829419+Abhishek9111@users.noreply.github.com>
2023-05-08 10:19:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+//decorator
+import { Todo } from './todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-list-angular';
+
+  todoValue: string;
+  list: Todo[];
+  // constructor() {
+  //   console.log('constructor called');
+  //   this.doSomething('money');
+  // }
+
+  // doSomething(val: string): void {
+  //   console.log('function called', val);
+  // }
+
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = '';
+  }
+
+  addItem() {
+    if (this.todoValue != '') {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false,
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = '';
+  }
+  deleteItem(id: number) {
+    this.list = this.list.filter((item) => item.id != id);
+  }
 }
